e7b84ff4acdacc191fc9fd120b9bb176f5e322ef
Fleks61 <133789192+Fleks61@users.noreply.github.com>
2026-05-15 17:53:11 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,18 +1,16 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
 class Train {
-private:
+ private:
     struct Car {
         bool light;
 
         Car* next;
         Car* prev;
 
-        explicit Car(bool state = false) {
-            light = state;
-
+        Car() {
+            light = false;
             next = nullptr;
             prev = nullptr;
         }
@@ -22,7 +20,7 @@ private:
 
     int countOp;
 
-public:
+ public:
     Train();
 
     ~Train();
@@ -31,7 +29,7 @@ public:
 
     int getLength();
 
-    int getOpCount() const;
+    int getOpCount();
 };
 
-#endif  
+#endif  // INCLUDE_TRAIN_H_
