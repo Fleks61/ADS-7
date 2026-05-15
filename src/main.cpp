d632dbca363e75526718cfc93ca35599e71de31b
Fleks61 <133789192+Fleks61@users.noreply.github.com>
2026-05-15 17:13:39 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,68 @@
-// Copyright 2022 NNTU-CS
 #include <iostream>
+#include <random>
+
 #include "train.h"
 
-int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+using namespace std;
 
-  while (count--)
-    train.addCar(false);
+void experimentAllOff(int n)
+{
+    Train train;
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    for (int i = 0; i < n; i++)
+    {
+        train.addCar(false);
+    }
+
+    train.getLength();
+
+    cout << n << " " << train.getOpCount() << endl;
+}
+
+void experimentAllOn(int n)
+{
+    Train train;
+
+    for (int i = 0; i < n; i++)
+    {
+        train.addCar(true);
+    }
+
+    train.getLength();
+
+    cout << n << " " << train.getOpCount() << endl;
+}
+
+void experimentRandom(int n)
+{
+    Train train;
+
+    random_device rd;
+
+    mt19937 gen(rd());
+
+    uniform_int_distribution<> dist(0, 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        train.addCar(dist(gen));
+    }
+
+    train.getLength();
+
+    cout << n << " " << train.getOpCount() << endl;
+}
+
+int main()
+{
+    for (int n = 2; n <= 100; n += 10)
+    {
+        experimentAllOff(n);
+
+        experimentAllOn(n);
+
+        experimentRandom(n);
+    }
+
+    return 0;
 }
