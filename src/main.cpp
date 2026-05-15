67ef92839b14b18b3a9345cfa1e3e26834e224d4
Fleks61 <133789192+Fleks61@users.noreply.github.com>
2026-05-15 17:53:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,61 @@
 // Copyright 2025
+
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
-#include <random>
 
 #include "train.h"
 
-void experimentAllOff(int n) {
-    Train train;
-
-    for (int i = 0; i < n; i++) {
-        train.addCar(false);
-    }
-
-    train.getLength();
-
-    std::cout << n << " " << train.getOpCount() << std::endl;
-}
-
-void experimentAllOn(int n) {
-    Train train;
-
-    for (int i = 0; i < n; i++) {
-        train.addCar(true);
-    }
-
-    train.getLength();
-
-    std::cout << n << " " << train.getOpCount() << std::endl;
-}
-
-void experimentRandom(int n) {
-    Train train;
-
-    std::random_device rd;
-
-    std::mt19937 gen(rd());
-
-    std::uniform_int_distribution<> dist(0, 1);
-
-    for (int i = 0; i < n; i++) {
-        train.addCar(dist(gen));
-    }
-
-    train.getLength();
-
-    std::cout << n << " " << train.getOpCount() << std::endl;
-}
-
 int main() {
-    for (int n = 2; n <= 100; n += 10) {
-        experimentAllOff(n);
+    std::srand(static_cast<unsigned>(time(nullptr)));
 
-        experimentAllOn(n);
+    std::cout << "n false true random\n";
 
-        experimentRandom(n);
+    for (int n = 2; n <= 100; n++) {
+        int opFalse;
+        int opTrue;
+        int opRandom;
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(false);
+            }
+
+            train.getLength();
+
+            opFalse = train.getOpCount();
+        }
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(true);
+            }
+
+            train.getLength();
+
+            opTrue = train.getOpCount();
+        }
+
+        {
+            Train train;
+
+            for (int i = 0; i < n; i++) {
+                train.addCar(std::rand() % 2);
+            }
+
+            train.getLength();
+
+            opRandom = train.getOpCount();
+        }
+
+        std::cout << n << " "
+                  << opFalse << " "
+                  << opTrue << " "
+                  << opRandom << std::endl;
     }
 
     return 0;
