5cad29f16fabbe1b968d45610ef3a8db804bf58c
Fleks61 <133789192+Fleks61@users.noreply.github.com>
2026-05-15 17:12:41 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,83 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train()
+{
+    first = nullptr;
+
+    countOp = 0;
+}
+
+Train::~Train()
+{
+    if (first == nullptr)
+        return;
+
+    Car* current = first->next;
+
+    while (current != first)
+    {
+        Car* temp = current;
+
+        current = current->next;
+
+        delete temp;
+    }
+
+    delete first;
+}
+
+void Train::addCar(bool light)
+{
+    Car* newCar = new Car(light);
+
+    if (first == nullptr)
+    {
+        first = newCar;
+
+        first->next = first;
+        first->prev = first;
+    }
+    else
+    {
+        Car* last = first->prev;
+
+        last->next = newCar;
+
+        newCar->prev = last;
+
+        newCar->next = first;
+
+        first->prev = newCar;
+    }
+}
+
+int Train::getOpCount() const
+{
+    return countOp;
+}
+
+int Train::getLength()
+{
+    if (first == nullptr)
+        return 0;
+
+    countOp = 0;
+
+    int length = 1;
+
+    Car* current = first->next;
+
+    countOp++;
+
+    while (current != first)
+    {
+        current = current->next;
+
+        length++;
+
+        countOp++;
+    }
+
+    return length;
+}
